c5dd8fa86f5b4f3183dd6936a047a38f656f5c47
sofia singh <sofiasingh4548@gmail.com>
2022-03-25 14:05:27 -0400

    Sum Whole Numbers

---
+++ src/com/company/Calculate.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Calculate {
+    public static void sumWholeNumbers()
+    {
+        int sum=0;
+        for(int i=0;i<=10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println("The Sum of Whole Numbers is "+sum);
+    }
+
+}
